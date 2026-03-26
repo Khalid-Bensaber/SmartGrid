76248ee4ba96142d358bc9ea3995323e16906924
Reda Chafik <115711333+redayreda8@users.noreply.github.com>
2026-03-26 07:34:24 +0100

    Update data_loader.py

--- src/smartgrid/data_loader.py
+++ src/smartgrid/data_loader.py
@@ -1,1 +1,13 @@
+import pandas as pd
+import numpy as np
 
+def load_data():
+    df = pd.read_csv("data/raw/historique.csv")
+
+    df = df.select_dtypes(include=[np.number])
+
+    data = df.iloc[-1].values
+
+    data = data[:15]
+
+    return data.astype(np.float32)
