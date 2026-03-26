9814a9d6edbc135d64a1c2e15a462e1a38c10e8c
Reda Chafik <115711333+redayreda8@users.noreply.github.com>
2026-03-26 07:57:53 +0100

    Update predict.py

--- src/smartgrid/predict.py
+++ src/smartgrid/predict.py
@@ -2,13 +2,13 @@ import numpy as np
 from src.smartgrid.data_loader import load_data
 
 def run_prediction(model):
-    # 👉 récupérer les données
+    
     data = load_data()
 
-    # 👉 reshape pour modèle
+    
     data = data.reshape(1, 1, -1)
 
-    # 👉 prédiction
+   
     prediction = model.predict(data)
 
     return {
