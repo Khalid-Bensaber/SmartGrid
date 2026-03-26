88970b65ec5d6a526a04862f22635a12c2bd59e0
Reda Chafik <115711333+redayreda8@users.noreply.github.com>
2026-03-26 06:49:57 +0100

    Update service.py

--- src/smartgrid/service.py
+++ src/smartgrid/service.py
@@ -1,6 +1,13 @@
+from smartgrid.model_loader import load_model
+from smartgrid.predict import run_prediction
+
 def predict_conso():
-    print("🔥 SERVICE EXECUTÉ 🔥")
+    print("🔥 MODEL EXECUTION 🔥")
+
+    model = load_model()
+    result = run_prediction(model)
+
     return {
         "status": "ok",
-        "prediction": [999]
+        "prediction": result
     }
