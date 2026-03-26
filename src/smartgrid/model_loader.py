44dd5b380e0c632b04332f20d0fa3622b79f115d
Reda Chafik <115711333+redayreda8@users.noreply.github.com>
2026-03-26 07:02:54 +0100

    Update model_loader.py

--- src/smartgrid/model_loader.py
+++ src/smartgrid/model_loader.py
@@ -1,5 +1,11 @@
 import tensorflow as tf
+import os
 
 def load_model():
-    model = tf.keras.models.load_model("../data/model.h5")
+    base_dir = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
+    model_path = os.path.join(base_dir, "data", "model.h5")
+
+    print("MODEL PATH:", model_path)
+
+    model = tf.keras.models.load_model(model_path)
     return model
