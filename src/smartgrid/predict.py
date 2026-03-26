be439ff296031e286b84d6fbaec3739567942709
Reda Chafik <115711333+redayreda8@users.noreply.github.com>
2026-03-26 06:49:32 +0100

    Update predict.py

--- src/smartgrid/predict.py
+++ src/smartgrid/predict.py
@@ -1,6 +1,9 @@
 import numpy as np
 
 def run_prediction(model):
+    # données fake pour test
     data = np.random.rand(1, 15)
+
     prediction = model.predict(data)
+
     return prediction.tolist()
