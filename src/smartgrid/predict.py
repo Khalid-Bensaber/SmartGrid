c25d29d8ece248cdd309e9d0a65465ab86554c7b
Reda Chafik <115711333+redayreda8@users.noreply.github.com>
2026-03-26 07:23:21 +0100

    Update predict.py

--- src/smartgrid/predict.py
+++ src/smartgrid/predict.py
@@ -1,9 +1,17 @@
 import numpy as np
 
 def run_prediction(model):
-    # données fake pour test
-    data = np.random.rand(1, 15)
+    # 👉 données de test (tu peux changer plus tard)
+    data = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
 
+    # 👉 convertir en numpy
+    data = np.array(data)
+
+    # 👉 reshape pour correspondre au modèle (batch, time_steps, features)
+    data = data.reshape(1, 1, -1)
+
+    # 👉 prédiction
     prediction = model.predict(data)
 
+    # 👉 convertir en liste pour JSON
     return prediction.tolist()
